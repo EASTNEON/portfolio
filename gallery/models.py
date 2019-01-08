5a5e28aa9170d4d395f4fc92d0933f786fe6b6ff
Unknown <cjl7042@qq.com>
2019-01-08 20:35:43 +0800

    完善了Gallery App
    设置好了Media路径和网址

--- gallery/models.py
+++ gallery/models.py
@@ -4,4 +4,10 @@ from django.db import models
 
 
 class Gallery(models.Model):
-    description = models.CharField(max_length=100)
+    # default为默认选项
+    description = models.CharField(default='在这里写作品的简介', max_length=100)
+    image = models.ImageField(default='default.jpg', upload_to='image/')
+    titlee = models.CharField(default='作品标题', max_length=50)
+
+    def __str__(self):
+        return self.titlee
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gallery.apps.GalleryConfig',#注册app
+    # 注册app
+    'gallery.apps.GalleryConfig',
 
 ]
 
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 媒体网址
+MEDIA_URL = '/media/'
+
+# 媒体根目录
+MEDIA_ROOT = os.path.join(BASE_DIR, 'meida')
